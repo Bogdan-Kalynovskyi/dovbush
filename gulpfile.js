b7a2074f5b5d44efc2a072ac313f568d66893847
easy-one <mybodya@gmail.com>
2016-11-07 22:44:48 +0100

    add SCSS compilation

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['sass'], function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
